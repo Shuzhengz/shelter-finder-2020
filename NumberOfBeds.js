afa975e9480f80dd2e00869064d326b1af639d09
jackie777seven <jo73198@djusdstudents.org>
2020-11-20 18:55:39 -0800

    Add empty pages

---
+++ NumberOfBeds.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NumberOfBeds = (props) => {
+    return (
+         <p>This is the number of beds page.</p>
+    )
+}
+
+export default NumberOfBeds
---
+++ Settings.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Settings = (props) => {
+    return (
+         <p>This is the settings page.</p>
+    )
+}
+
+export default Settings
