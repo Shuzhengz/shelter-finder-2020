83270d3df7324b3ae5ae41d55a165082f470ab2e
jackie777seven <jo73198@djusdstudents.org>
2020-11-22 11:28:12 -0800

    Moved new pages to src folder

--- Map.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Map = (props) => {
-    return (
-         <p>This is the map page.</p>
-    )
-}
-
-export default Map
--- NumberOfBeds.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const NumberOfBeds = (props) => {
-    return (
-         <p>This is the number of beds page.</p>
-    )
-}
-
-export default NumberOfBeds
